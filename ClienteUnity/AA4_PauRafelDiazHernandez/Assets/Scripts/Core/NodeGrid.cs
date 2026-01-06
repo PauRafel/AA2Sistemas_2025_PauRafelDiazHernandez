00bbaa00fa7de24c1cb9cead36b67634e1751f98
Pau Rafel Díaz Hernández <paurafel.diaz@enti.cat>
2026-01-06 12:19:12 +0100

    NodeGrid actualizado

--- ClienteUnity/AA4_PauRafelDiazHernandez/Assets/Scripts/Core/NodeGrid.cs
+++ ClienteUnity/AA4_PauRafelDiazHernandez/Assets/Scripts/Core/NodeGrid.cs
@@ -22,6 +22,7 @@ public class NodeGrid : MonoBehaviour
 
         public int x, y;
         public JewelType type;
+
         public Node(JewelType type, int x, int y)
         {
             this.type = type;
@@ -29,47 +30,51 @@ public class NodeGrid : MonoBehaviour
             this.y = y;
         }
     }
-    
+
     [Serializable]
     public class Grid
     {
-	    [Serializable]
-	    public class Column
-	    {
-		    public List<Node> nodes = new ();
-	    }
-	    
-	    public List<Column> columns = new ();
+        [Serializable]
+        public class Column
+        {
+            public List<Node> nodes = new List<Node>();
+        }
 
-	    [SerializeField]
-	    private int _playerId;
-	    public int PlayerId => _playerId;
-	    
-	    [SerializeField]
-	    private string _playerName;
-	    public string PlayerName => _playerName;
+        public List<Column> columns = new List<Column>();
+
+        [SerializeField]
+        private int _playerId;
+        public int PlayerId => _playerId;
+
+        [SerializeField]
+        private string _playerName;
+        public string PlayerName => _playerName;
 
         public Grid(GridSetup gridSetup)
         {
-	        _playerId = gridSetup.playerId;
-	        _playerName = gridSetup.playerName;
+            _playerId = gridSetup.playerId;
+            _playerName = gridSetup.playerName;
 
             for (int x = 0; x < gridSetup.sizeX; x++)
-	        {
-		        columns.Add(new ());
-	            for (int y = 0; y < gridSetup.sizeY; y++)
-	            {
-		            columns[x].nodes.Add(new Node(Node.JewelType.None,x, y));
-	            }
-	        }
-	    }
+            {
+                columns.Add(new Column());
+                for (int y = 0; y < gridSetup.sizeY; y++)
+                {
+                    columns[x].nodes.Add(new Node(Node.JewelType.None, x, y));
+                }
+            }
+        }
 
-	    public Node GetNode(int x, int y)
-	    {
-		    return columns[x].nodes[y];
-	    }
+        public Node GetNode(int x, int y)
+        {
+            if (x >= 0 && x < columns.Count && y >= 0 && y < columns[x].nodes.Count)
+            {
+                return columns[x].nodes[y];
+            }
+            return null;
+        }
     }
-    
+
     [Serializable]
     public class GridUpdate
     {
@@ -87,64 +92,168 @@ public class NodeGrid : MonoBehaviour
         public int sizeY;
     }
 
+    [Header("Grid Settings")]
+    [SerializeField] private int gridSizeX = 6;
+    [SerializeField] private int gridSizeY = 12;
+    [SerializeField] private float cellSize = 1f;
+    [SerializeField] private GameObject cellPrefab;
+
+    [Header("Colors")]
+    [SerializeField] private Color colorRed = Color.red;
+    [SerializeField] private Color colorGreen = Color.green;
+    [SerializeField] private Color colorBlue = Color.blue;
+    [SerializeField] private Color colorYellow = Color.yellow;
+    [SerializeField] private Color colorOrange = new Color(1f, 0.5f, 0f);
+    [SerializeField] private Color colorPurple = new Color(0.5f, 0f, 1f);
+    [SerializeField] private Color colorEmpty = new Color(0.2f, 0.2f, 0.2f);
+
     private Grid _grid;
+    private GameObject[,] visualCells;
 
     public void SetupGrid(GridSetup gridSetup)
     {
-	    //TODO Add Code Here
-	    /*
-	        This function must be called from the server connection script.
-	        Here you need to implement the necessary logic so that your visual grid implementation for setup the grid
-	    */
-	    _grid = new Grid(gridSetup);
-	}
+        _grid = new Grid(gridSetup);
+        gridSizeX = gridSetup.sizeX;
+        gridSizeY = gridSetup.sizeY;
 
-	public void UpdateGrid(GridUpdate gridUpdate)
-	{
-	    //TODO Add Code Here
-	    /*
-	        This function must be called from the server connection script.
-	        Here you need to implement the necessary logic so that your visual grid implementation is updated and the changes can be displayed.
+        CreateVisualGrid();
 
-	        It is recommended to add variables to the classes defined above to facilitate integration.
-	     */
+        Debug.Log($"Grid setup: {gridSetup.playerName} ({gridSizeX}x{gridSizeY})");
     }
 
-    private void Start()
+    public void UpdateGrid(GridUpdate gridUpdate)
     {
-        //The code shown below is an example of how to convert GridUpdate objects to JSON and vice versa.
-
-        SetupGrid(new()
+        if (_grid == null)
         {
-	        playerId = 0,
-	        playerName = "P1",
-	        sizeX = 6,
-	        sizeY = 12
+            Debug.LogWarning("Grid not initialized");
+            return;
+        }
+
+        foreach (var node in gridUpdate.updatedNodes)
+        {
+            if (node.x >= 0 && node.x < gridSizeX && node.y >= 0 && node.y < gridSizeY)
+            {
+                _grid.columns[node.x].nodes[node.y].type = node.type;
+                UpdateVisualCell(node.x, node.y, node.type);
+            }
+        }
+    }
+
+    void CreateVisualGrid()
+    {
+        if (visualCells != null)
+        {
+            for (int x = 0; x < visualCells.GetLength(0); x++)
+            {
+                for (int y = 0; y < visualCells.GetLength(1); y++)
+                {
+                    if (visualCells[x, y] != null)
+                    {
+                        Destroy(visualCells[x, y]);
+                    }
+                }
+            }
+        }
+
+        visualCells = new GameObject[gridSizeX, gridSizeY];
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 position = new Vector3(x * cellSize, y * cellSize, 0);
+
+                GameObject cell;
+                if (cellPrefab != null)
+                {
+                    cell = Instantiate(cellPrefab, position, Quaternion.identity, transform);
+                }
+                else
+                {
+                    cell = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cell.transform.position = position;
+                    cell.transform.localScale = new Vector3(cellSize * 0.9f, cellSize * 0.9f, 0.1f);
+                    cell.transform.parent = transform;
+                }
+
+                cell.name = $"Cell_{x}_{y}";
+                visualCells[x, y] = cell;
+
+                UpdateVisualCell(x, y, Node.JewelType.None);
+            }
+        }
+
+        CenterCamera();
+    }
+
+    void UpdateVisualCell(int x, int y, Node.JewelType type)
+    {
+        if (visualCells == null || visualCells[x, y] == null) return;
+
+        var renderer = visualCells[x, y].GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.color = GetColorForJewelType(type);
+        }
+    }
+
+    Color GetColorForJewelType(Node.JewelType type)
+    {
+        switch (type)
+        {
+            case Node.JewelType.Red: return colorRed;
+            case Node.JewelType.Green: return colorGreen;
+            case Node.JewelType.Blue: return colorBlue;
+            case Node.JewelType.Yellow: return colorYellow;
+            case Node.JewelType.Orange: return colorOrange;
+            case Node.JewelType.Purple: return colorPurple;
+            default: return colorEmpty;
+        }
+    }
+
+    void CenterCamera()
+    {
+        Camera mainCam = Camera.main;
+        if (mainCam != null)
+        {
+            float centerX = (gridSizeX - 1) * cellSize / 2f;
+            float centerY = (gridSizeY - 1) * cellSize / 2f;
+
+            mainCam.transform.position = new Vector3(centerX, centerY, -15f);
+
+            if (mainCam.orthographic)
+            {
+                mainCam.orthographicSize = (gridSizeY * cellSize) / 1.5f;
+            }
+        }
+    }
+
+    [ContextMenu("Test Grid")]
+    void TestGrid()
+    {
+        SetupGrid(new GridSetup
+        {
+            playerId = 1,
+            playerName = "Test Player",
+            sizeX = 6,
+            sizeY = 12
         });
 
-        string json = JsonUtility.ToJson(_grid);
-        
-        Debug.Log(json);
-
-        Grid g = JsonUtility.FromJson<Grid>(json);
-
-        GridUpdate update = new()
+        GridUpdate testUpdate = new GridUpdate
         {
-	        playerId = 0,
-	        playerName = "P1",
-	        updatedNodes = new()
+            playerId = 1,
+            playerName = "Test Player",
+            updatedNodes = new List<Node>
+            {
+                new Node(Node.JewelType.Red, 0, 11),
+                new Node(Node.JewelType.Green, 1, 11),
+                new Node(Node.JewelType.Blue, 2, 11),
+                new Node(Node.JewelType.Yellow, 3, 11),
+                new Node(Node.JewelType.Orange, 4, 11),
+                new Node(Node.JewelType.Purple, 5, 11)
+            }
         };
-        
-        update.updatedNodes.Add(new Node(Node.JewelType.Red,0,1));
-        update.updatedNodes.Add(new Node(Node.JewelType.Green,0,2));
-        update.updatedNodes.Add(new Node(Node.JewelType.Blue,0,3));
-        update.updatedNodes.Add(new Node(Node.JewelType.None,0,4));
-        
-        string json2 = JsonUtility.ToJson(update);
-        
-        Debug.Log(json2);
 
-        GridUpdate update2 = JsonUtility.FromJson<GridUpdate>(json2);
+        UpdateGrid(testUpdate);
     }
-    
 }
